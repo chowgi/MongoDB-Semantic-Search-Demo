3092381f7e5c0366d5bb2d6fb271c6414450f6ac
BenKarciauskas <14050015-BenKarciauskas@users.noreply.replit.com>
2025-02-15 02:37:03 +0000

    User checkpoint: Update README and app to reflect changes; remove dog image.

--- main.py
+++ main.py
@@ -24,17 +24,17 @@ def setup_db():
 db = setup_db()
 
 # Initialize FastHTML with MonsterUI theme
-app, rt = fast_app(hdrs=Theme.blue.headers(daisy=True), static_path="static", debug=True)
+app, rt = fast_app(hdrs=Theme.blue.headers(daisy=True), static_path="public", debug=True)
 
 #App routes
 @rt("/")
 def home():
-    return Titled("Your First App",
+    return Titled("FastHTML and MongoDB Template",
       Card(
           H1("Welcome!"),
-          P("Your first MonsterUI app", cls=TextPresets.muted_sm),
-          P("I'm excited to see what you build with MonsterUI!"),
-          Img(src="/static/dog.jpg", alt="Dog picture", cls="w-full rounded-lg")))
+          P("Your first FastHTML and MongoDB app", cls=TextPresets.muted_sm),
+          P("I'm excited to see what you build with FastHTML and MongoDB!"),
+          Img(src="dog.jpg", alt="Dog picture", cls="w-full rounded-lg")))
           
 
 serve()
